6fda57fb643ccf76710b54a4785772bdf9da2823
richardzhangy26 <77674465+richardzhangy26@users.noreply.github.com>
2021-02-01 10:45:57 +0800

    Add files via upload

---
+++ max_drawdown.py
@@ -0,0 +1,13 @@
+from math import sqrt
+# 随机数random.random()
+from random import random
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+
+minlist = [1,2,3,4]
+
+print(type(min(minlist)))
+mink = []
+mink.append(max(minlist))
+print(mink)
